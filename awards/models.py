ed27a8b2537d1dafef7d3ca56bd9bcf9c6691b93
michael otieno <michael.otieno@student.moringaschool.com>
2021-10-23 02:08:20 +0300

    add project class

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models.fields import URLField
+from django.db.models.fields.files import ImageField
+
+
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/', blank=True)
+    description = models.TextField()
+    url = URLField(max_length=100)
+    

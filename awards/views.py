d4015d4a082107ca7d782adf7b6c5f55337f8b6a
michael otieno <michael.otieno@student.moringaschool.com>
2021-10-23 13:54:23 +0300

    add signup view

--- awards/views.py
+++ awards/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import SignUpForm, PostProjectForm, ProfileForm,Profile
+from django.contrib.auth.models import User
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            user = User.objects.create_user(username=username,
+                                            password=password,
+                                            first_name=first_name,
+                                            last_name=last_name,
+                                            email=email)
+            profile = Profile.objects.create(user=user)
+
+            return HttpResponseRedirect('/accounts/login')
+
+    else:
+        form = SignUpForm()
+
+    return render(request, 'registration/signup.html', {'form': form})
+

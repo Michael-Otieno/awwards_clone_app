7380c02376fad9cbe40fc07ac966b0ff17a656f2
michael otieno <michael.otieno@student.moringaschool.com>
2021-10-23 17:53:53 +0300

    add search_project view

--- awards/views.py
+++ awards/views.py
@@ -2,7 +2,7 @@ from django import template
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 
-from awards.models import Comment, Project
+from awards.models import Comment, Like, Project
 from .forms import SignUpForm, PostProjectForm, ProfileForm,Profile
 from django.contrib.auth.models import User
 from django.template import context, loader
@@ -102,4 +102,84 @@ def add_comment(request):
             return redirect(reverse('home'))
 
 def like_project(request, projectid):
+    project = Project.objects.get(id=projectid)
     try:
+        is_Liked = Like.objects.get(project_linked=project, user__username=request.user.username)
+        Like.objects.filter(project_linked=project, user__username=request.user.username).delete()
+        project.likes -=1
+
+    except Like.DoesNotExist:
+        Like.objects.create(project_linked=project, user=request.user)
+        project.likes +=1
+
+    project.save()
+    return redirect(reverse('home'))
+
+def add_project(request):
+    template = loader.get_template('award/project.html')
+    profile = Profile.objects.get(user=request.user)
+    if request.method == 'POST':
+        profile = Profile.objects.get(user=request.user)
+        form = PostProjectForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            fs = form.save(commit=False)
+            fs.author = profile
+            fs.save()
+            return redirect(reverse('home'))
+
+    else:
+        form = PostProjectForm()
+        pass
+
+    context={'form':form, 'profile':profile}
+    return HttpResponse(template.render(context, request))
+
+def edit_profile(request, username):
+    template = loader.get_template('award/edit_profile.html')
+    user = User.objects.get(username=request.user.username)
+    profile = Profile.objects.get(user=request.user)
+
+    if request.meyhod == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            user.username = form.cleaned_data['username']
+            user.first_name = form.cleaned_data['first_name']
+            user.save()
+            profile.biography = form.cleaned_data['biography']
+            profile.profile_pic = form.cleaned_data['profile_pic']
+            profile.contact = form.cleaned_data['contact']
+            profile.save()
+            return redirect(reverse('home'))
+
+    else:
+        form = ProfileForm(initial={
+                                    'username': username,
+                                    'first_name': user.first_name,
+                                    'last_name': user.last_name,
+                                    'email':user.email,
+                                    'biography': profile.biography})
+
+    context = {'form':form, 'user':user, 'profile':profile}
+    return HttpResponse(template.render(context, request))
+
+def search_project(request):
+    if 'search_project' in request.GET and request.GET['search_project']:
+        title = request.GET.get('search_project')
+        results = Project.search_project(title)
+        print(results)
+        message = f'title'
+        params = {
+            'results':results,
+            'message':message
+        }
+        return render(request, 'award/result.html', params)
+
+    else:
+        message='Project not found'
+
+    return render(request, 'award/result.html', {'message':message})
+
+
+
+    

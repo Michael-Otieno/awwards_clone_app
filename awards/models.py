2407c2f41e0519c0911d6773dfeff928cd86ff33
michael otieno <michael.otieno@student.moringaschool.com>
2021-10-23 11:47:05 +0300

    add profile model

--- awards/models.py
+++ awards/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 from django.db.models.fields import URLField
 from django.db.models.fields.files import ImageField
-
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Project(models.Model):
-    title = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='images/', blank=True)
+    title = models.CharField(max_length=100,null=True, blank=True, default="title")
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
     description = models.TextField()
     url = URLField(max_length=100)
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
+    profile_pic = models.ImageField(upload_to ='images/',null=True, blank=True)
+    biography = models.TextField(blank=True)
+    contact = models.EmailField(max_length=100, blank=True)

8f772b7084ebdb4043cbee4b452c6c3c4e638ef7
michael otieno <michael.otieno@student.moringaschool.com>
2021-10-25 00:08:40 +0300

    add url for logout

--- awards/views.py
+++ awards/views.py
@@ -47,7 +47,7 @@ def register(request):
         email=request.POST['email']
         password1=request.POST['password1']
         password2=request.POST['password2']
-        user = User.objects.create_user(username=username,email=email,password=password1,password2=password2)
+        user = User.objects.create_user(username=username,email=email,password=password1)
         user.save()
         profile=Profile.objects.create(user=user,email=user.email)
         
